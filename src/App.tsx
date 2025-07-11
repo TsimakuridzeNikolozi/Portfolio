863d2a52265e51a05ca0d99ac76c442f6f75807e
TsimakuridzeNikolozi <ntsim20@freeuni.edu.ge>
2025-07-12 00:29:34 +0400

    Added sections structure and project assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Contact from './sections/Contact';
+import Education from './sections/Education';
+import Hero from './sections/Hero';
+import Projects from './sections/Projects';
+import Skills from './sections/Skills';
+import WorkExperience from './sections/WorkExperience';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <WorkExperience />
+      <Education />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
